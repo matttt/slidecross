028fda7c2ff7504845ed41b119a1d2856e9f7102
Matthew Martori <matthew.martori@gmail.com>
2024-01-18 18:43:45 -0800

    remove extraneous useState

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from 'react-dom/client';
 import "./layout/index.css";
 import reportWebVitals from "./game/reportWebVitals.js";
