83b322a5b37b2a822cee43059c973891e1d144b8
Matthew Martori <matthew.martori@gmail.com>
2024-01-23 17:21:19 -0800

    email

--- src/layout/MainMenu.jsx
+++ src/layout/MainMenu.jsx
@@ -31,7 +31,8 @@ export const MainMenu = () => {
     <div className="flex flex-col justify-center items-center" style={{ width: "100%" }}>
       <h1 className="text-4xl font-bold mb-4 text-center md:text-left" style={{ color: 'white', marginTop: "50px" }}>slidecross</h1>
       <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>a puzzle game by <a href="https://twitter.com/mateomartori" style={{ color: 'white' }}>matthew martori</a></h4>
-      <h4 className="text-xs mb-4 text-center md:text-left" style={{ color: 'white' }}>puzzles by alex tuchi</h4>
+      <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>puzzles by alex tuchi</h4>
+      <h4 className="text-xs mb-4 text-center md:text-left" style={{ color: 'white' }}><a href="mailto:slidecrossdev@gmail.com">email us!</a></h4>
 
       <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>smallies</h2>
       <div className="grid grid-cols-3 gap-4 md:grid-cols-8">
