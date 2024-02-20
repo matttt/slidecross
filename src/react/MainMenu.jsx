2506d33c2dd3c0484e227e854ec65270f47c445d
Matthew Martori <matthew.martori@gmail.com>
2024-02-19 16:29:47 -0800

    bring email back to top

--- src/react/MainMenu.jsx
+++ src/react/MainMenu.jsx
@@ -38,6 +38,8 @@ export const MainMenu = () => {
       <h1 className="text-4xl font-bold mb-4 text-center md:text-left" style={{ color: 'white', marginTop: "50px" }}>slidecross</h1>      
       <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>a puzzle game by matthew martori</h4>
       <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>puzzles by alex tuchi</h4>
+      <h4 className="text-xs mb-4 text-center md:text-left underline" style={{ color: 'white' }}><a href="mailto:slidecrossdev@gmail.com">email us!</a></h4>
+
 
       <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>3x3</h2>
       <div className="grid grid-cols-4 gap-1 md:grid-cols-4">
@@ -68,7 +70,6 @@ export const MainMenu = () => {
       <div className="grid grid-cols-1 gap-2 md:grid-cols-3 mb-10">{createPuzzleButtonArr`fifteens`}</div>
 
     
-      <h4 className="text-xs mb-4 text-center md:text-left underline" style={{ color: 'white' }}><a href="mailto:slidecrossdev@gmail.com">email us!</a></h4>
     </div>
   );
 };
