fe25029751a178159bf1d74171641b91f86133a6
Matthew McMillan <git@matthewmcmillan.me>
2024-03-18 21:17:24 -0400

    Add GitHub link to homepage, move some styles to Tailwind

--- src/react/MainMenu.jsx
+++ src/react/MainMenu.jsx
@@ -23,6 +23,10 @@ export const MainMenu = () => {
     </Button>;
   };
 
+  const createPuzzleHeader = n => (
+    <h2 className="text-2xl text-center text-white md:text-left mt-3">{n}x{n}</h2>
+  )
+
   // where category matches a section name in game/puzzles.js
   const createPuzzleButtonArr = (category) => puzzles[category].map((p, i) => createPuzzleButton(p, i, category));
 
@@ -34,39 +38,40 @@ export const MainMenu = () => {
   // const fifteensButtons = puzzles.biggies.map((p,i) => createPuzzleButton(p, i, 'fifteensButtons'));
 
   return (
-    <div className="flex flex-col justify-center items-center" style={{ width: "100%" }}>
-      <h1 className="text-4xl font-bold mb-4 text-center md:text-left" style={{ color: 'white', marginTop: "50px" }}>slidecross</h1>
-      <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>a puzzle game by matthew martori</h4>
-      <h4 className="text-xs mb-1 text-center md:text-left" style={{ color: 'white' }}>puzzles by alex tuchi</h4>
-      <h4 className="text-xs mb-4 text-center md:text-left underline" style={{ color: 'white' }}><a href="mailto:slidecrossdev@gmail.com">email us!</a></h4>
+    <div className="flex flex-col justify-center items-center w-full">
+      <h1 className="text-4xl font-bold mb-4 mt-12 text-center text-white md:text-left">slidecross</h1>
+      <h4 className="text-xs mb-1 text-center text-white md:text-left">a puzzle game by matthew martori</h4>
+      <h4 className="text-xs mb-1 text-center text-white md:text-left">puzzles by alex tuchi</h4>
+      <h4 className="text-xs mb-1 text-center text-white md:text-left underline"><a href="mailto:slidecrossdev@gmail.com">email us!</a></h4>
+      <h4 className="text-xs mb-4 text-center text-white md:text-left underline"><a href="https://github.com/matttt/slidecross">see the code</a></h4>
 
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>3x3</h2>
+      {createPuzzleHeader(3)}
       <div className="grid grid-cols-4 gap-1 md:grid-cols-4">
         {createPuzzleButtonArr`threes`}
       </div>
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>4x4</h2>
+      {createPuzzleHeader(4)}
       <div className="grid grid-cols-4 gap-1 md:grid-cols-4">
         {createPuzzleButtonArr`fours`}
       </div>
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>5x5</h2>
+      {createPuzzleHeader(5)}
       <div className="grid grid-cols-3 gap-4 md:grid-cols-4">
         {createPuzzleButtonArr`fives`}
       </div>
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>9x9</h2>
+      {createPuzzleHeader(9)}
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
         {createPuzzleButtonArr`nines`}
       </div>
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>11x11</h2>
+      {createPuzzleHeader(11)}
       <div className="grid grid-cols-2 gap-4 md:grid-cols-2">
         {createPuzzleButtonArr`elevens`}
       </div>
 
-      <h2 className="text-2xl text-center md:text-left mt-3" style={{ color: 'white' }}>15x15</h2>
+      {createPuzzleHeader(15)}
       <div className="grid grid-cols-1 gap-2 md:grid-cols-3 mb-10">{createPuzzleButtonArr`fifteens`}</div>
 
       {/* <h4 className="text-xs mt-4 text-center md:text-left" style={{ color: 'white' }}>slidecross © 2023 by Matthew Martori is licensed under CC BY-NC-SA 4.0 </h4> */}
